4b8748a9291564b9ca23df8998117e253f84a49e
victor_workspace <victor@aws_workspace>
2023-02-03 16:39:06 -0600

    update source and executable file

--- exercise_1/returnFizzbuzz.cc
+++ exercise_1/returnFizzbuzz.cc
@@ -3,29 +3,20 @@
 
 using namespace std; 
 
-string fizzBuzz(){
-  return "fizzbuzz"; 
-}
-
-string fizz(){
-  return "fizz"; 
-}
-
-string buzz(){
-  return "buzz";
+void fizzBuzz(int& n){
+  if(n%3 == 0 && n%5 == 0)
+    cout << "fizzbuzz" << endl;
+  else if(n%3 == 0)
+    cout << "fizz" << endl; 
+  else if(n%5 == 0)
+    cout << "buzz" << endl;
+  else 
+    cout << n << endl; 
 }
 
 int main(){
-  for(int n=1; n<=50; n++){
-    if(n%3 == 0 && n%5 == 0)
-      fizzBuzz();
-    else if((n%3) == 0)
-      fizz();
-    else if((n%5) == 0)
-      buzz();
-    else
-      cout << n << endl;
-  }
-  return 0;
-}
-
+  for(int n=1; n<=50; ++n){
+    fizzBuzz(n);
+ }
+ return 0;
+} 
